e42428556fefe7be7b1ab9a9e2c7815c43cba9f9
Sebastien Beauvais <sbeauvais7@gmail.com>
2022-10-06 09:58:44 -0700

    added 100 day rolling avg

--- homework/assignment_3/spark_connection.py
+++ homework/assignment_3/spark_connection.py
@@ -1,5 +1,10 @@
+# libraries
+import sys
+
+from pyspark.sql import SparkSession
+
+
 def main():
-    from pyspark.sql import SparkSession
 
     appName = "PySpark Connection"
     master = "local"
@@ -11,15 +16,17 @@ def main():
         SUM(bc.Hit) AS total_hits,
         SUM(bc.atBat) AS total_atBats,
         SUM(bc.Hit)/SUM(bc.atBat) AS batting_avg,
-        DATE(g.local_date) AS ora_date
+        DATE(g.local_date) AS ora_date,
+        AVG(SUM(bc.Hit)/SUM(bc.atBat)) OVER (ORDER BY g.game_id, DATE(g.local_date)
+            ROWS BETWEEN 100 PRECEDING AND 1 PRECEDING) AS rolling_avg
         FROM batter_counts bc
         JOIN game g
         ON g.game_id = bc.game_id
-        GROUP BY g.game_id
-        ORDER BY g.game_id DESC"""
+        GROUP BY g.game_id"""
     database = "baseball"
     user = "root"
-    password = ""  # your password
+    # pragma: allowlist nextline secret
+    password = "password"  # your password
     server = "localhost"
     port = 3306
     jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
@@ -31,13 +38,14 @@ def main():
         .option("url", jdbc_url)
         .option("query", sql)
         .option("user", user)
+        # pragma: allowlist nextline secret
         .option("password", password)
         .option("driver", jdbc_driver)
         .load()
     )
 
-    df.show(10)
+    df.show(15)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
