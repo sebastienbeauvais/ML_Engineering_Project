0f3da9e44da50d8b6f913d0ab7a51341068cb15e
Sebastien Beauvais <sbeauvais7@gmail.com>
2022-10-12 13:18:56 -0700

    pushing working code

--- homework/assignment_4/hw4.py
+++ homework/assignment_4/hw4.py
@@ -1,25 +1,12 @@
-# libs
+# libraries
 import sys
-from io import StringIO
 
 import pandas as pd
-import pydot
 
-# using diabetes as initial test data
-# code ripped from slides atm
-# rename stats to sm
-# import statsmodels.api
-# from pandas import DataFrame
-# from plotly import express as px
-# from plotly import graph_objects as go
-# from sklearn import datasets
-# from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+# import statsmodels.api as sm
 
 
-#######################################
-# model building code                 #
-#######################################
+# function to printing  headings
 def print_heading(title):
     print("*" * 80)
     print(title)
@@ -27,105 +14,70 @@ def print_heading(title):
     return
 
 
-def plot_decision_tree(decision_tree, feature_names, class_names, file_out):
-    with StringIO() as dot_data:
-        export_graphviz(
-            decision_tree,
-            feature_names=feature_names,
-            class_names=class_names,
-            out_file=dot_data,
-            filled=True,
-        )
-        graph = pydot.graph_from_dot_data(dot_data.getvalue())
-        graph[0].write_pdf(file_out + ".pdf")  # must access graphs first element
-        graph[0].write_png(file_out + ".png")  # must access graphs first element
+# function to load any sklearn dataset in as pandas dataframe
+def sklearn_to_df(sklearn_dataset):
+    df = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
+    df["target"] = pd.Series(sklearn_dataset.target)
+    return df
 
 
-# main
+# first pass testing on titanic
 def main():
-    #######################################
-    # model building code
-    #######################################
-    # Increase pandas print viewport (so we see more on the screen)
-    pd.set_option("display.max_rows", 60)
-    pd.set_option("display.max_columns", 500)
-    pd.set_option("display.width", 1_000)
+    df_titanic = pd.read_csv("./datasets/titanic.csv")
+    print(df_titanic.head())
 
-    # Load the famous fishers iris data set
-    fishers_iris_df = pd.read_csv(
-        "https://teaching.mrsharky.com/data/iris.data", header=None
-    )
+    # split for titanic
+    column_to_move = df_titanic.pop("Survived")  # remove survived - dependent variable
+    df_titanic[
+        "Survived"
+    ] = column_to_move  # add survived (DV) to end of df to split easier
 
-    # renaming columns for better readability
-    fishers_iris_df = fishers_iris_df.rename(
-        columns={
-            0: "sepal_width",
-            1: "sepal_length",
-            2: "petal_width",
-            3: "petal_length",
-            4: "class",
-        }
-    )
+    # removing useless columns
+    df_titanic.pop("Ticket")
+    df_titanic.pop("Cabin")
+    df_titanic.pop("PassengerId")
+    df_titanic.pop("Name")
 
-    # drop rows with missing vals
-    fishers_iris_df = fishers_iris_df.dropna()
+    X = df_titanic.iloc[:, :-1]
+    y = df_titanic.iloc[:, -1]
 
-    # checking data frame
-    print(fishers_iris_df.head())
-
-    # printing column names
-    for col in fishers_iris_df.columns:
-        print(col)
-
-    print_heading("Original Dataset")
-    print(fishers_iris_df)
-
-    # Continuous Features
-    continuous_features = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
-    X = fishers_iris_df[continuous_features].values
-
-    # Response
-    y = fishers_iris_df["class"].values
-
-    # Decision Tree Classifier
-    max_tree_depth = 7
-    tree_random_state = 0  # Always set a seed
-    decision_tree = DecisionTreeClassifier(
-        max_depth=max_tree_depth, random_state=tree_random_state
-    )
-    decision_tree.fit(X, y)
-
-    # Plot the decision tree
-    plot_decision_tree(
-        decision_tree=decision_tree,
-        feature_names=continuous_features,
-        class_names="classification",
-        file_out="./plots/hw_iris_tree_full",
-    )
-    # breaking df into response(dependent) and predictors(independent)
-    response = fishers_iris_df.iloc[:, -1]  # last column
-    predictors = fishers_iris_df.iloc[:, :-1]  # all but last column
-    print(response.name)
-    print(predictors.columns)
-
-    # loop through each predictor and check continuous or boolean
-    for (columnName, columnData) in predictors.iteritems():
+    # categorizing each independent (predictor) variable
+    print_heading("Predictors")
+    catg = pd.DataFrame()
+    cont = pd.DataFrame()
+    for (columnName, columnData) in X.iteritems():
         print("Column Name : ", columnName)
         print("Unique Values : ", columnData.nunique())
-        if columnData.nunique() > 4:
-            print("Continuous\n")
-        else:
+        if columnData.nunique() <= 3:
+            catg[columnName] = columnData
             print("Boolean\n")
+        elif columnData.dtype == "object" and columnData.nunique() <= 3:
+            catg[columnName] = columnData
+            print("Boolean\n")
+        else:
+            cont[columnName] = columnData
+            print("Continuous\n")
 
-    # Count unique values of response to categorize as boolean or continuous
-    print("Column Name : ", response.name)
-    print("Unique Values : ", response.nunique())
-    if (
-        response.nunique() > 4
-    ):  # we just need to check for 2 but in this example we have 3
+    # checking catg/cont
+    print(catg.head())
+    print(cont.head())
+
+    # categorizing dependent variable
+    print_heading("Dependent Variable")
+    print("Column Name : ", y.name)
+    print("Unique Values : ", y.nunique())
+    if y.nunique() > 2:  # we just need to check for 2
         print("Continuous")
     else:
         print("Boolean")
+
+    # get list of column headers to iterate over
+    column_headers = list(X.columns)
+
+    for index, col in enumerate(X.T):
+        feature_name = column_headers[index]
+        print(feature_name)
+        # predictor = sm.add_constant(X.iloc[:, col])
 
 
 if __name__ == "__main__":
