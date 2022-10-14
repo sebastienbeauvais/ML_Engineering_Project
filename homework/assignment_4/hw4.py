4267abde0e1fb8cc579ba9e73c531f2488f6dc65
Sebastien Beauvais <sbeauvais7@gmail.com>
2022-10-14 10:28:51 -0700

    added first pass decision tree classifier. Working on pruned tree

--- homework/assignment_4/hw4.py
+++ homework/assignment_4/hw4.py
@@ -1,11 +1,18 @@
 # libraries
 import sys
+from io import StringIO
 
 import pandas as pd
 import plotly.express as px
+import pydot
 
 # import plotly.graph_objects as go
 import statsmodels.api as sm
+
+# from pandas import DataFrame
+# from plotly import graph_objects as go
+# from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 
 # function to for printing headings
@@ -21,6 +28,20 @@ def sklearn_to_df(sklearn_dataset):
     df = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
     df["target"] = pd.Series(sklearn_dataset.target)
     return df
+
+
+def plot_decision_tree(decision_tree, feature_names, class_names, file_out):
+    with StringIO() as dot_data:
+        export_graphviz(
+            decision_tree,
+            feature_names=feature_names,
+            class_names=class_names,
+            out_file=dot_data,
+            filled=True,
+        )
+        graph = pydot.graph_from_dot_data(dot_data.getvalue())
+        graph[0].write_pdf(file_out + ".pdf")  # must access graph's first element
+        graph[0].write_png(file_out + ".png")  # must access graph's first element
 
 
 # first pass testing on titanic
@@ -87,36 +108,51 @@ def main():
         print(f"Variable: {titanic_features}")
         print(lm_fitted.summary())
 
-        # Get the stats
+        # Get the p/t stats
         t_value = round(lm_fitted.tvalues[1], 6)
         p_value = "{:.6e}".format(lm_fitted.pvalues[1])
 
         # Plot the figure
-        fig = px.scatter(x=col, y=y, trendline="ols")
+        fig = px.scatter(x=y, y=col, trendline="ols")
         fig.update_layout(
             title=f"Variable: {titanic_features}: (t-value={t_value}) (p-value={p_value})",
-            xaxis_title=f"Variable: {titanic_features}",
-            yaxis_title="y",
+            xaxis_title=f"y: {y.name}",
+            yaxis_title=f"Variable: {titanic_features}",
         )
-        fig.show()
-
-    # getting p and t values
-    # t_value = round(lm_fitted.tvalues[1], 6)
-    # p_value = "{:.6e}".format(lm_fitted.pvalues[1])
+        # fig.show()
 
     # checking relationship between variables
     print(X.corr())
-
-    # converts object types to dummies
-    """for key in X:
-        print("key: ", key)
-        if X[key].dtype == 'object':
-            X = pd.get_dummies(X, columns=[key])"""
 
     # plotting each predictor against dependent
     for key in X:
         fig = px.density_heatmap(df_titanic, x=key, y=y, height=500, width=500)
         # fig.show()
+
+    # converts object types to dummies for random forest classifier
+    dummies_df = pd.get_dummies(X)
+
+    print(dummies_df.head())
+
+    cont_features = list(dummies_df.columns)
+    dummies_X = dummies_df[cont_features].values
+    dummies_y = y
+
+    # decision tree classifier
+    max_tree_depth = 7
+    tree_random_state = 0  # setting a seed for reproduction
+    decision_tree = DecisionTreeClassifier(
+        max_depth=max_tree_depth, random_state=tree_random_state
+    )
+    decision_tree.fit(dummies_X, dummies_y)
+
+    # Plot the decision tree
+    plot_decision_tree(
+        decision_tree=decision_tree,
+        feature_names=cont_features,
+        class_names="classification",
+        file_out="titanic_tree_full",
+    )
 
 
 if __name__ == "__main__":
